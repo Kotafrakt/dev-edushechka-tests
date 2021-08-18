27add1a498cf921e62b136f13597e80d04b86808
Timur <Kotafrakt@gmail.com>
2021-08-18 20:15:43 +0300

    fix namespace

--- IntegrationTests/DevEdu.Tests/Facades/TagControllerFacade.cs
+++ IntegrationTests/DevEdu.Tests/Facades/TagControllerFacade.cs
@@ -6,7 +6,9 @@ namespace DevEdu.Tests.Facades
     public class TagControllerFacade
     {
         private TagCreator _creator;
+
         public TagControllerFacade() { _creator = new TagCreator(); }
+
         internal TagOutputModel AddTag(string token)
         {
             return _creator.AddTag(token);
